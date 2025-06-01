f965c476be6885924cba212d0ed3257536457d48
Дмитро <singurikdima80@email.com>
2025-06-01 14:52:20 +0300

    fix

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
-app.title = "Оцінка вартості картки"
+app.title = "Оцінка вартості картки"    
+
+server = app.server
 
 app.layout = dbc.Container([
     html.Br(),
